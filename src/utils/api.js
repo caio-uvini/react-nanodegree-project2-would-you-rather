33b27ace9386f23265a6b9f8a932e43f52bfcdef
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-10-17 13:02:24 -0300

    feat: Expose _DATA functions as api

---
+++ src/utils/api.js
@@ -0,0 +1,9 @@
+import { _getUsers, _getQuestions, _saveQuestion, _saveQuestionAnswer } from './_DATA.js'
+
+
+const getUsers = _getUsers
+const getQuestions = _getQuestions
+const saveQuestion = _saveQuestion
+const saveQuestionAnswer = _saveQuestionAnswer
+
+export { getUsers, getQuestions, saveQuestion, saveQuestionAnswer }
