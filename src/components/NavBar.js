f77ea9d15c00b4702ec40e3d1d30446a5c8b382e
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-10-18 14:46:51 -0300

    feat: NavBar initial impl

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const NavBar = () => {
-	return (
-		<div>NavBar</div>
-	)
+import * as AuthedUserActions from '../actions/authedUser';
+
+import * as AuthedUserSelectors from '../selectors/authedUser';
+import * as UsersSelectors from '../selectors/users';
+
+const NavBar = (props) => {
+
+  const {authedUser, signOut} = props;
+
+  return (
+    <div>
+      <a href="/">Home</a>
+      <a href="/">New Question</a>
+      <a href="/">Leaderboard</a>
+      { authedUser && (
+        <div>
+          Welcome back {authedUser.name}! 
+          <a href="/" onClick={() => signOut()}>Sign Out</a>
+        </div>
+    )}
+    </div>
+  )
 }
 
-export default NavBar;
+const mapStateToProps = (state) => {
+
+  const authedUserId = AuthedUserSelectors.getCurrent(state);
+  if (!authedUserId) {
+    return {}
+  }
+
+  const authedUser = UsersSelectors.getUserForDisplay(state, authedUserId);
+
+  return {
+    authedUser: authedUser
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  signOut: () => dispatch(AuthedUserActions.signOut())
+})
+
+const NavBarContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NavBar)
+
+export default NavBarContainer;
--- src/selectors/users.js
+++ src/selectors/users.js
@@ -3,11 +3,20 @@ const rootState = (state) => state.usersById;
 function getUsersForDisplay(state) {
   const usersState = rootState(state);
   return Object.keys(usersState)
-    .map((userId) => ({
-      id: userId,
-      name: usersState[userId].name,
-      avatarURL: usersState[userId].avatarURL
-    }))
+    .map((userId) => formatForDisplay(usersState[userId]));
+}
+
+function getUserForDisplay(state, userId) {
+  const usersState = rootState(state);
+  return formatForDisplay(usersState[userId]);
+}
+
+function formatForDisplay(user) {
+  return {
+    id: user.id,
+    name: user.name,
+    avatarURL: user.avatarURL
+  }
 }
 
 function getAnswers(state, userId) {
@@ -15,4 +24,4 @@ function getAnswers(state, userId) {
   return usersState[userId].answers;
 }
 
-export { getUsersForDisplay, getAnswers }
+export { getUserForDisplay, getUsersForDisplay, getAnswers }
