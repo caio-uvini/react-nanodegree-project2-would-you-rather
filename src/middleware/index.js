79c0d283e9847dabc103b7f2ed515b7655ac9f6d
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-10-18 12:10:20 -0300

    feat: Add middlewares

---
+++ src/middleware/index.js
@@ -0,0 +1,6 @@
+import logger from './logger'
+import thunk from 'redux-thunk';
+import { applyMiddleware } from 'redux';
+
+const middleware = applyMiddleware(thunk, logger)
+export default middleware
