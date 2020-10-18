f82b2882a90b409a10e97a4c7074b582d8949cd0
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-10-18 12:17:17 -0300

    feat: Create redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './components/App';
+import reducers from './reducers';
+import middleware from './middleware';
+
+const store = createStore(reducers, middleware);
 
 ReactDOM.render(
   <React.StrictMode>
-  	<App />
+  	<Provider store={store}>
+  	  <App />
+   	</Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
