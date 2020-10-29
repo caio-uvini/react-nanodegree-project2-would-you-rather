41ab353d6ff146d784f6c58aa90bb5db4efde321
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-10-28 21:48:53 -0300

    refactor: Add prop-types to NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const NotFound = ({ msg }) => {
   return (
     <div className='not-found'>{msg}</div>
   );
+};
+
+NotFound.propTypes = {
+  msg: PropTypes.string.isRequired
 };
 
 export default NotFound;
